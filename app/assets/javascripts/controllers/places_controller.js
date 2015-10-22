a441941d4be129bf71d5917decd48d51b8c274a2
ba1uev <ofseawave@gmail.com>
2015-10-22 00:58:19 -0700

    review, betta

--- app/assets/javascripts/controllers/places_controller.js
+++ app/assets/javascripts/controllers/places_controller.js
@@ -1,10 +1,11 @@
 Topdoner.controller('PlacesCtrl', function ($scope,places) {
-  $scope.places = places
+  $scope.places = places;
 //  $scope.func = function(place){
 //    console.log(place)
 //  }
-  $scope.openPlacePopup = function(place){
-      $scope.current_place = place.properties.title
+  $scope.choosePlace = function(place){
+//      $scope.current_place = place.properties.title
+	  console.log('yas');
   };
 
   $scope.closePlacePopup = function(){
