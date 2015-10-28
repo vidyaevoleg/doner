fcb7ba2b2308e40b560750f59f9f80ed61dd7ae5
ba1uev <ofseawave@gmail.com>
2015-10-28 01:43:54 -0700

    e

--- app/assets/javascripts/controllers/new_place_controller.js
+++ app/assets/javascripts/controllers/new_place_controller.js
@@ -3,7 +3,7 @@ Topdoner.controller('NewPlaceCtrl', ['$scope','places','$location','$rootScope',
 
   $scope.createPlace = function(){
     var new_place = $scope.new_place
-//    places.createPlace(new_place)
-	console.log(new_place);
+    places.createPlace(new_place)
+//	console.log(new_place);
   }
 }]);
