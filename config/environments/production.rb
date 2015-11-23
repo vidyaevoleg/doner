cd9eb056bb1eb13133d3a104789e547289cc1cf2
vidyaevoleg <olezhka-vidyayev@mail.ru>
2015-11-23 23:46:39 +0300

    heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,8 @@
-Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
+R  # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-
+  config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.css *.scss *.js *.eot *.woff *.ttf *.svg)
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
@@ -29,12 +28,14 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
-
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
+  config.serve_static_files = true
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Specifies the header that your server uses for sending files.
@@ -47,6 +48,8 @@ Rails.application.configure do
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
   config.log_level = :debug
+  config.assets.version = '1.0'
+  config.log_level = :info
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
@@ -66,6 +69,11 @@ Rails.application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
+  config.action_mailer.raise_delivery_errors = true
+  
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+
   config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners.
@@ -73,6 +81,7 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
