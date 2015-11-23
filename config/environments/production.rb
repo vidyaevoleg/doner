1d1eece0776d5325ee44dfd09ba1e32f4c9859e4
vidyaevoleg <olezhka-vidyayev@mail.ru>
2015-11-24 00:01:39 +0300

    hotfix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
-R  # Settings specified here will take precedence over those in config/application.rb.
+Rails.application.configure do
+ # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
   config.cache_classes = true
