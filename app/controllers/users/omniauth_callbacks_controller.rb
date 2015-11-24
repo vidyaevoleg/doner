e3761af28f0adf2d5885b71aa804611409cfed10
vidyaevoleg <olezhka-vidyayev@mail.ru>
2015-11-24 21:46:56 +0300

    hz

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,7 +1,6 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def vkontakte
-    binding.pry
   	@user = User.find_for_vkontakte_oauth request.env["omniauth.auth"]
     if @user.persisted?
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Vkontakte"
