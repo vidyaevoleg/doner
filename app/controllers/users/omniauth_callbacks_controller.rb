fc54cf032e0c139e4c673fafd84bdc6accdfecc1
vidyaevoleg <olezhka-vidyayev@mail.ru>
2015-11-27 00:00:03 +0300

    pizdec

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,14 +1,16 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def vkontakte
-  	@user = User.find_for_vkontakte_oauth request.env["omniauth.auth"]
-    if @user.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Vkontakte"
-      sign_in @user, :event => :authentication
-    else
-      flash[:notice] = "authentication error"
-      redirect_to root_path
-    end
+    redirect_to 'http://yandex.ru'
+
+  	# @user = User.find_for_vkontakte_oauth request.env["omniauth.auth"]
+   #  if @user.persisted?
+   #    flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Vkontakte"
+   #    sign_in @user, :event => :authentication
+   #  else
+   #    flash[:notice] = "authentication error"
+   #    redirect_to root_path
+   #  end
   end
 
   def facebook
